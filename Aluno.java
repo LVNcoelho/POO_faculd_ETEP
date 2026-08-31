b80512e432bcf6d393930dca8bae163ea06f9e1a
Pâmela Coelho <pamelacoelho117@gmail.com>
2026-08-31 08:28:01 -0300

    Refactor Aluno class for encapsulation and methods
    Refactored Aluno class to use encapsulation with private attributes and added constructors and getter/setter methods.

--- Aluno.java
+++ Aluno.java
@@ -1,8 +1,64 @@
 public class Aluno {
-    // Atributos solicitados
-    String nome;
-    String enderecoResidencial;
-    String telefoneContato;
-    String email;
-    String matricula;
+    
+    // 1. Atributos protegidos (ENCAPSULAMENTO)
+    private String nome;
+    private String enderecoResidencial;
+    private String telefoneContato;
+    private String email;
+    private String matricula;
+
+    // 2. Construtor Vazio (permite criar 'new Aluno()' e usar Setters depois)
+    public Aluno() {
+    }
+
+    // 3. Construtor Completo (preenche tudo usando o 'this')
+    public Aluno(String nome, String enderecoResidencial, String telefoneContato, String email, String matricula) {
+        this.nome = nome;
+        this.enderecoResidencial = enderecoResidencial;
+        this.telefoneContato = telefoneContato;
+        this.email = email;
+        this.matricula = matricula;
+    }
+
+    // 4. Métodos GETTERS e SETTERS
+
+    // Nome
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Endereço Residencial
+    public String getEnderecoResidencial() {
+        return this.enderecoResidencial;
+    }
+    public void setEnderecoResidencial(String enderecoResidencial) {
+        this.enderecoResidencial = enderecoResidencial;
+    }
+
+    // Telefone Contato
+    public String getTelefoneContato() {
+        return this.telefoneContato;
+    }
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    // E-mail
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Matrícula
+    public String getMatricula() {
+        return this.matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
